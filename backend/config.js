beb8ccb6880deb7208fd042dec84cf45483d93f1
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-12-26 22:38:03 +0530

    updated

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://ashug6332:ashutosh@cluster0.wrtfc3h.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
