ebb6b07c440c85343bdc88c4386784389e09d3c8
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-12-28 00:05:21 +0530

    building and deploying

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+import path from 'path'
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 // Middleware for handling CORS POLICY
 app.use(cors());
@@ -20,10 +23,14 @@ app.use(cors());
 //   })
 // );
 
-app.get('/', (request, response) => {
-  console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
-});
+// app.get('/', (request, response) => {
+//   console.log(request);
+//   return response.status(234).send('Welcome To MERN Stack Tutorial');
+// });
+
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+})
 
 app.use('/books', booksRoute);
 
